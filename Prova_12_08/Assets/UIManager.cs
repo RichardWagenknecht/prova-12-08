00eafb7a895d0f76154dbbb91335ff301e87d8c6
ipysk <sarahjuliaantunesv@gmail.com>
2024-08-12 08:58:16 -0300

    1. Criação da Classe UIManager

--- Prova_12_08/Assets/UIManager.cs
+++ Prova_12_08/Assets/UIManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static UIManager Instance;
+
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] TextMeshProUGUI sequenciaTexto;
+    [SerializeField] TextMeshProUGUI acertouTexto;
+    [SerializeField] TextMeshProUGUI errouTexto;
+    [SerializeField] Button[] botoes;
+
+  
 }
