e0791d75e71dd3b66be500d413036ea4b2427f24
ipysk <sarahjuliaantunesv@gmail.com>
2024-08-12 10:14:03 -0300

    arrumando scripts

--- Prova_12_08/Assets/Scripts/GameManager.cs
+++ Prova_12_08/Assets/Scripts/GameManager.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    int corDaVez, acertos, erros;
+
+    int[] sequencia;
+
+    [SerializeField] string[] nomes;
+    private void Start()
     {
-        
+        GerarSequencia();
+    }
+    private void GerarSequencia()
+    {
+        corDaVez = 0;
+
+        sequencia = new int [Random.Range(3, nomes.Length)];
+
+        UIManager.instance.LimparTextos;
+
+        for (int i = 0; i < sequencia.Length; i++)
+        {
+            sequencia = new int [Random.Range(0, nomes.Length)];
+            UIManager.instance.AtualizarSequencia(nomes[sequencia[i]]);
+        }
+    }
+    private void ChecarCor(int corIndex)
+    {
+        if (corIndex == sequencia[corDaVez])
+        {
+            corDaVez++;
+
+            if (corDaVez == sequencia.Length)
+            {
+                acertos++;
+
+                UIManager.instance.AtualizarAcertos();
+
+                GerarSequencia ();
+            }
+        }
+        else
+        {
+            erros ++;
+
+            UIManager.instance.AtualizarErros();
+            GerarSequencia();
+        }
     }
 }
--- Prova_12_08/Assets/Scripts/UIManager.cs
+++ Prova_12_08/Assets/Scripts/UIManager.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.Build.Content;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static UIManager Instance;
+
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] TextMeshProUGUI sequenciaTexto;
+    [SerializeField] TextMeshProUGUI acertouTexto;
+    [SerializeField] TextMeshProUGUI errouTexto;
+    [SerializeField] Button[] botoes;
+
+    public void Start()
     {
-        
+        for (int i = 0; i < botoes.Length; i++)
+        {
+            int x = i;
+            botoes[i].onClick = GameManager.instance.Checarcor(x); 
+        }
+
     }
+
+
+    public void AtualizarAcertos(int acertos)
+    {
+        acertouTexto.text = acertos.ToString();
+    }
+
+    public void AtualizarErros (int erros)
+    {
+        errouTexto.text = erros.ToString();
+    }
+    public void LimparTexto()
+    {
+        sequenciaTexto.text = string.Empty;
+    }
+
+    public void AtualizarSequencia (string cor)
+    {
+     sequenciaTexto.text = cor + " ";
+    }
+
 }
+
